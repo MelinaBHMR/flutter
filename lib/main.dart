73c235775f1c76c33e8a5ccdd61d5b49c68b398c
Melina BRUHAMMER <melina.bruhammer@niji.fr>
2020-07-07 17:12:21 +0200

    Config permissions et changements nom projet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/presentation/widgets/repairer_map_widget.dart';
+import 'package:flutter_openstreetmap_project/presentation/widgets/repairer_map_widget.dart';
 
 
 void main() {
--- lib/presentation/widgets/repairer_map_widget.dart
+++ lib/presentation/widgets/repairer_map_widget.dart
@@ -1,10 +1,10 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/presentation/page/repairer_map_page.dart';
+import 'package:flutter_openstreetmap_project/presentation/page/repairer_map_page.dart';
 
 class MyApp extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
